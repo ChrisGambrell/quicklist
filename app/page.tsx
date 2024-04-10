8de6e7d2c15d792b4c8689e62f3885a8c51014da
Chris Gambrell <christopher@gambrell.info>
2024-04-10 18:02:44 -0400

    Init supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function RootPage() {
-	return <div>hello, world!</div>
+	return (
+		<div className='max-w-lg border shadow rounded-lg mx-auto mt-20 p-8'>
+			<h1 className='font-black text-3xl tracking-tight'>Hello, world!</h1>
+		</div>
+	)
 }
