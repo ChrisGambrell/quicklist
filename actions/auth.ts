3d13e9711a819f9fb94b6579094aea6218868220
Chris Gambrell <christopher@gambrell.info>
2024-04-27 10:08:14 -0400

    Add sign in with github button

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,18 +2,19 @@
 
 import { createClient } from '@/utils/supabase/server'
 import { ActionReturn } from '@/utils/types'
+import { Provider } from '@supabase/supabase-js'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
-const signInSchema = z.object({ email: z.string().email(), password: z.string() })
+const signInWithPasswordSchema = z.object({ email: z.string().email(), password: z.string() })
 const signUpSchema = z.object({ name: z.string().min(1), email: z.string().email(), password: z.string() })
 const passwordResetSchema = z.object({ email: z.string().email() })
 
-export async function signIn(_prevState: any, formData: FormData): Promise<ActionReturn<typeof signInSchema>> {
+export async function signInWithPassword(_prevState: any, formData: FormData): Promise<ActionReturn<typeof signInWithPasswordSchema>> {
 	const data = Object.fromEntries(formData)
 
-	const parsed = signInSchema.safeParse(data)
+	const parsed = signInWithPasswordSchema.safeParse(data)
 	if (!parsed.success) return { errors: parsed.error.flatten().fieldErrors }
 
 	const supabase = createClient()
@@ -23,6 +24,21 @@ export async function signIn(_prevState: any, formData: FormData): Promise<Actio
 
 	revalidatePath('/', 'layout')
 	redirect('/')
+}
+
+export async function signInWithOAuth(provider: Provider): Promise<ActionReturn<undefined>> {
+	const supabase = createClient()
+
+	// TODO: When cancelling the OAuth flow, the user is redirected to /auth/confirm with an error message that should be handled by toaster http://127.0.0.1:3000/sign-in#error=access_denied&error_description=The+user+has+denied+your+application+access.
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider,
+		options: { redirectTo: `${process.env.NEXTAUTH_URL}/auth/callback` },
+	})
+	if (error || !data) return { errors: { _global: [error?.message || 'An unknown error occured'] } }
+
+	redirect(data.url)
+
+	// http://127.0.0.1:54321/auth/v1/verify?token=pkce_f70a94ee6ea9994c53f590335206d4c164e895fb75164b557a46eaac&type=recovery&redirect_to=http://127.0.0.1:3000/auth/reset
 }
 
 export async function signUp(prevState: any, formData: FormData): Promise<ActionReturn<typeof signUpSchema>> {
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { signIn } from '@/actions/auth'
+import { signInWithOAuth, signInWithPassword } from '@/actions/auth'
 import ActionButton from '@/components/action-button'
 import { FormError, useErrorToaster } from '@/components/form'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
@@ -10,8 +10,12 @@ import Link from 'next/link'
 import { useFormState } from 'react-dom'
 
 export default function SignInPage() {
-	const [state, action] = useFormState(signIn, null)
-	useErrorToaster(state?.errors?._global)
+	const [passwordState, passwordAction] = useFormState(signInWithPassword, null)
+	useErrorToaster(passwordState?.errors?._global)
+
+	const useSignInWithGithub = signInWithOAuth.bind(null, 'github')
+	const [githubState, githubAction] = useFormState(useSignInWithGithub, null)
+	useErrorToaster(githubState?.errors?._global)
 
 	return (
 		<Card className='mx-auto max-w-sm border-0 shadow-none sm:border sm:shadow-sm sm:my-20'>
@@ -20,30 +24,29 @@ export default function SignInPage() {
 				<CardDescription>Enter your email below to sign in to your account</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<form className='grid gap-4' action={action}>
+				<form className='grid gap-4'>
 					<div className='grid gap-2'>
 						<Label htmlFor='email'>Email</Label>
 						<Input id='email' name='email' type='email' placeholder='m@example.com' />
-						<FormError errors={state?.errors} id='email' />
+						<FormError errors={passwordState?.errors} id='email' />
 					</div>
 					<div className='grid gap-2'>
 						<div className='flex items-center'>
 							<Label htmlFor='password'>Password</Label>
-							{/* TODO: forgot password */}
 							<Link href='/forgot' className='ml-auto inline-block text-sm underline'>
 								Forgot your password?
 							</Link>
 						</div>
 						<Input id='password' name='password' type='password' />
-						<FormError errors={state?.errors} id='password' />
+						<FormError errors={passwordState?.errors} id='password' />
 					</div>
-					<ActionButton type='submit' className='w-full'>
+					<ActionButton formAction={passwordAction} type='submit' className='w-full'>
 						Sign in
 					</ActionButton>
 					{/* TODO: log in with google */}
-					{/* <Button variant='outline' className='w-full'>
-						Login with Google
-					</Button> */}
+					<ActionButton formAction={githubAction} variant='outline' className='w-full'>
+						Sign in with Github
+					</ActionButton>
 				</form>
 				<div className='mt-4 text-center text-sm'>
 					Don&apos;t have an account?{' '}
