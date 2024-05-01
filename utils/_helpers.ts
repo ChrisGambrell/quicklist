83c9aab985e1fccb9547e65a0405b0bad9601305
Chris Gambrell <christopher@gambrell.info>
2024-05-01 10:30:12 -0400

    Fix "sub-claim" message on first access

--- utils/_helpers.ts
+++ utils/_helpers.ts
@@ -7,11 +7,10 @@ export async function getAuth() {
 	const supabase = createClient()
 
 	const { data: auth, error: authError } = await supabase.auth.getUser()
-	if (authError || !auth.user)
-		redirect(getErrorRedirect('/sign-in', authError?.message ?? 'You need to be signed in to access this page'))
+	if (authError || !auth.user) redirect('/sign-in')
 
 	const { data: user, error: userError } = await supabase.from('users').select().eq('id', auth.user.id).maybeSingle()
-	if (userError || !user) redirect(getErrorRedirect('/sign-in', userError?.message))
+	if (userError || !user) redirect(getErrorRedirect('/sign-in', userError?.message ?? "FATAL: User's profile not found."))
 
 	return { auth: auth.user, user, supabase }
 }
