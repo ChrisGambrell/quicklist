898af54c15120180d41b8329976e342b421b9b9c
Chris Gambrell <christopher@gambrell.info>
2024-05-09 09:43:28 -0400

    Add types for tables

--- utils/types.ts
+++ utils/types.ts
@@ -7,15 +7,20 @@ export type ListingImage = Tables<'listing_images'>
 export type Rule = Tables<'rules'>
 export type User = Tables<'users'>
 
-export type ListingWithGenerationsAndImages = Listing & { generations: Generation[]; images: ListingImage[] }
-
 export type Price = Tables<'prices'>
 export type Product = Tables<'products'>
 export type ProductAmount = Tables<'product_amounts'>
 export type Purchase = Tables<'purchases'>
 export type Subscription = Tables<'subscriptions'>
 
+export type ListingWithGenerationsAndImages = Listing & { generations: Generation[]; images: ListingImage[] }
+
 export type ProductWithPrices = Product & { prices: Price[] }
+export type UserWithGenerationsAndPurchases = User & {
+	generations: Generation[]
+	purchases: (Purchase & { price: Price & { product: Product & { product_amount: ProductAmount } } })[]
+}
+
 export type SubscriptionWithPriceWithProductWithAmount = Subscription & {
 	price: Price & { product: Product & { amount: ProductAmount } }
 }
