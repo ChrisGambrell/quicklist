99c89b1ffce50c1acc7a636dcebafc4dbdd02616
Chris Gambrell <christopher@gambrell.info>
2024-05-09 09:39:25 -0400

    Make default state explicit so that I don't accidently have unhandled state

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
 	data: TData[]
-	defaultState?: Partial<TableState>
+	defaultState?: Partial<{ sorting: TableState['sorting'] }>
 }
 
 export function DataTable<TData, TValue>({ columns, data, defaultState }: DataTableProps<TData, TValue>) {
