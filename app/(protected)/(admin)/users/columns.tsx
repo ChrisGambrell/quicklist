c4743f6d4b945093958f7c76679449276e6dffe3
Chris Gambrell <christopher@gambrell.info>
2024-05-09 09:35:32 -0400

    Use new table for users page

---
+++ app/(protected)/(admin)/users/columns.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { ColumnHeader } from '@/components/column-header'
+import { PLACEHOLDER_AVATAR } from '@/utils/constants'
+import { UserWithGenerationsAndPurchases } from '@/utils/types'
+import { ColumnDef } from '@tanstack/react-table'
+import Image from 'next/image'
+
+export const columns: ColumnDef<UserWithGenerationsAndPurchases>[] = [
+	{
+		accessorKey: 'avatar_url',
+		header: '',
+		cell: ({ getValue }) => (
+			<Image
+				src={getValue<UserWithGenerationsAndPurchases['avatar_url']>() ?? PLACEHOLDER_AVATAR}
+				alt='Listing image'
+				className='aspect-square rounded-md object-cover'
+				height={64}
+				width={64}
+			/>
+		),
+		meta: { className: 'min-w-[64] sm:min-w-[100px] sm:table-cell' },
+	},
+	{
+		accessorKey: 'full_name',
+		header: ({ column }) => <ColumnHeader column={column} title='Name' />,
+		// FIXME: Pass row to className meta so I can determine isAdmin ? 'font-black' : 'font-medium'
+	},
+	{
+		id: 'purchased',
+		accessorFn: (row) => row.purchases.reduce((prev, curr) => prev + curr.price.product.product_amount.credits, 0),
+		header: ({ column }) => <ColumnHeader column={column} title='Purchased' />,
+	},
+	{
+		id: 'used',
+		accessorFn: (row) => row.generations.reduce((prev, curr) => prev + curr.credits, 0),
+		header: ({ column }) => <ColumnHeader column={column} title='Used' />,
+	},
+	{
+		accessorKey: 'email',
+		header: ({ column }) => <ColumnHeader column={column} title='Email' />,
+		meta: { className: 'hidden lg:table-cell' },
+	},
+	{
+		accessorKey: 'created_at',
+		header: ({ column }) => <ColumnHeader column={column} title='Created at' />,
+		cell: ({ getValue }) => new Date(getValue<UserWithGenerationsAndPurchases['created_at']>()).toDateString(),
+		meta: { className: 'hidden md:table-cell' },
+	},
+]
--- app/(protected)/(admin)/users/page.tsx
+++ app/(protected)/(admin)/users/page.tsx
@@ -1,10 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { cn } from '@/lib/utils'
-import { PLACEHOLDER_AVATAR } from '@/utils/constants'
+import { DataTable } from '@/components/data-table'
 import { createClient } from '@/utils/supabase/server'
+import { UserWithGenerationsAndPurchases } from '@/utils/types'
 import { Metadata } from 'next'
-import Image from 'next/image'
+import { columns } from './columns'
 
 export const metadata: Metadata = {
 	title: 'QuickList - Users',
@@ -14,65 +12,11 @@ export const metadata: Metadata = {
 export default async function UsersPage() {
 	const supabase = createClient()
 
-	const { data: users } = await supabase.from('users').select().order('created_at', { ascending: false })
-	const { data: generations } = await supabase.from('generations').select()
-	const { data: purchases } = await supabase
-		.from('purchases')
-		.select('*, price:prices(*, product:products(*, product_amount:product_amounts(*)))')
+	const { data: users } = await supabase
+		.from('users')
+		.select('*, generations(*), purchases(*, price:prices(*, product:products(*, product_amount:product_amounts(*))))')
+		.order('created_at', { ascending: false })
+		.returns<UserWithGenerationsAndPurchases[]>()
 
-	return (
-		<>
-			<Card>
-				<CardHeader>
-					<CardTitle>Users</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<Table>
-						<TableHeader>
-							<TableRow>
-								<TableHead className='w-[64px] sm:w-[100px] sm:table-cell'>
-									<span className='sr-only'>Avatar</span>
-								</TableHead>
-								<TableHead>Name</TableHead>
-								<TableHead>Purchased</TableHead>
-								<TableHead>Used</TableHead>
-								<TableHead className='hidden lg:table-cell'>Email address</TableHead>
-								<TableHead className='hidden md:table-cell'>Created at</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{users?.map((user) => (
-								<TableRow key={user.id}>
-									<TableCell className='min-w-[64px] sm:min-w-[100px]'>
-										<Image
-											src={user.avatar_url ? user.avatar_url : PLACEHOLDER_AVATAR}
-											alt='Listing image'
-											className='aspect-square rounded-md object-cover'
-											height={64}
-											width={64}
-										/>
-									</TableCell>
-									<TableCell className={cn(user.is_admin ? 'font-black' : 'font-medium')}>
-										{user.full_name ?? '-'}
-									</TableCell>
-									<TableCell>
-										{purchases
-											?.filter((purchase) => purchase.user_id === user.id)
-											.reduce((prev, curr) => prev + (curr.price?.product?.product_amount?.credits ?? 0), 0) ?? '-'}
-									</TableCell>
-									<TableCell>
-										{generations
-											?.filter((generation) => generation.user_id === user.id)
-											.reduce((prev, curr) => prev + curr.credits, 0) ?? '-'}
-									</TableCell>
-									<TableCell className='hidden lg:table-cell'>{user.email}</TableCell>
-									<TableCell className='hidden md:table-cell'>{new Date(user.created_at).toDateString()}</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</CardContent>
-			</Card>
-		</>
-	)
+	return <DataTable columns={columns} data={users ?? []} defaultState={{ sorting: [{ id: 'purchased', desc: true }] }} />
 }
