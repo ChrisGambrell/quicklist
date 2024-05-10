f6994ca220165d5632d98b76766702f8f15ebf5a
Chris Gambrell <christopher@gambrell.info>
2024-05-10 15:20:58 -0400

    Show which rows are shown, not how many are selected

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -11,7 +11,11 @@ export function Pagination<TData>({ table }: PaginationProps<TData>) {
 	return (
 		<div className='flex items-center justify-between px-2'>
 			<div className='flex-1 text-sm text-muted-foreground'>
-				{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
+				{/* {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected. */}
+				{`${table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1}-${Math.min(
+					(table.getState().pagination.pageIndex + 1) * table.getState().pagination.pageSize,
+					table.getFilteredRowModel().rows.length
+				)} rows shown`}
 			</div>
 			<div className='flex items-center space-x-6 lg:space-x-8'>
 				<div className='flex items-center space-x-2'>
@@ -25,7 +29,7 @@ export function Pagination<TData>({ table }: PaginationProps<TData>) {
 							<SelectValue placeholder={table.getState().pagination.pageSize} />
 						</SelectTrigger>
 						<SelectContent side='top'>
-							{[5, 10, 25, 50].map((pageSize) => (
+							{[5, 10, 25].map((pageSize) => (
 								<SelectItem key={pageSize} value={`${pageSize}`}>
 									{pageSize}
 								</SelectItem>
