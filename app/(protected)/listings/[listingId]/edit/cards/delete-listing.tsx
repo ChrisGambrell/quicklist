f543e8e9de6f42da0a72a8620ee8d96cfd01c691
Chris Gambrell <christopher@gambrell.info>
2024-12-10 08:29:17 -0600

    Use icon buttons and confirm delete for listing

--- app/(protected)/listings/[listingId]/edit/cards/delete-listing.tsx
+++ app/(protected)/listings/[listingId]/edit/cards/delete-listing.tsx
@@ -1,24 +1,23 @@
 import { deleteListing } from '@/actions/listing'
-import ActionButton from '@/components/action-button'
+import { ConfirmDelete } from '@/components/confirm-delete'
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Listing } from '@prisma/client'
 
 export default function DeleteListing({ listingId }: { listingId: Listing['id'] }) {
-	const useDeleteListing = deleteListing.bind(null, { listingId })
-
 	return (
-		<form action={useDeleteListing}>
-			<Card>
-				<CardHeader>
-					<CardTitle>Delete Listing</CardTitle>
-					<CardDescription>This cannot be reversed.</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<ActionButton className='w-full' size='sm' variant='destructive'>
+		<Card>
+			<CardHeader>
+				<CardTitle>Delete Listing</CardTitle>
+				<CardDescription>This cannot be reversed.</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ConfirmDelete action={deleteListing.bind(null, { listingId })}>
+					<Button className='w-full' size='sm' variant='destructive'>
 						Delete Listing
-					</ActionButton>
-				</CardContent>
-			</Card>
-		</form>
+					</Button>
+				</ConfirmDelete>
+			</CardContent>
+		</Card>
 	)
 }
--- app/(protected)/listings/columns.tsx
+++ app/(protected)/listings/columns.tsx
@@ -1,16 +1,16 @@
 'use client'
 
+import { deleteListing } from '@/actions/listing'
 import { ColumnHeader } from '@/components/column-header'
-import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { ConfirmDelete } from '@/components/confirm-delete'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { PLACEHOLDER_IMAGE } from '@/lib/constants'
-import { getImageUrl } from '@/lib/utils'
-import { Listing, Prisma } from '@prisma/client'
+import { cn, getImageUrl } from '@/lib/utils'
+import { Prisma } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
-import { MoreHorizontalIcon } from 'lucide-react'
+import { EditIcon, Trash2Icon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 type ColType = Prisma.ListingGetPayload<{ include: { images: true } }>
 
@@ -53,38 +53,19 @@ export const columns: ColumnDef<ColType>[] = [
 	{
 		id: 'actions',
 		header: () => <span className='sr-only'>Actions</span>,
-		cell: ({ row }) => <Actions listingId={row.original.id} />,
-		meta: { className: 'whitespace-nowrap' },
+		cell: ({ row }) => (
+			<div className='flex gap-1'>
+				<Link
+					className={cn(buttonVariants({ variant: 'outline', className: 'size-6 p-0' }))}
+					href={`/listings/${row.original.id}/edit`}>
+					<EditIcon className='size-4' />
+				</Link>
+				<ConfirmDelete action={deleteListing.bind(null, { listingId: row.original.id })}>
+					<Button className='size-6 p-0' variant='outline'>
+						<Trash2Icon className='size-4' />
+					</Button>
+				</ConfirmDelete>
+			</div>
+		),
 	},
 ]
-
-function Actions({ listingId }: { listingId: Listing['id'] }) {
-	const router = useRouter()
-
-	// async function deleteListing() {
-	// 	const supabase = createClient()
-
-	// 	const { error } = await supabase.from('listings').delete().eq('id', listingId)
-	// 	if (error) return toast.error(error.message)
-
-	// 	toast.success('Listing deleted')
-	// 	router.refresh()
-	// }
-
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button aria-haspopup='true' size='icon' variant='ghost'>
-					<MoreHorizontalIcon className='h-4 w-4' />
-					<span className='sr-only'>Toggle menu</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
-				<DropdownMenuLabel>Actions</DropdownMenuLabel>
-				<DropdownMenuItem onClick={() => router.push(`/listings/${listingId}/edit`)}>Edit</DropdownMenuItem>
-				{/* TODO: Delete listing */}
-				<DropdownMenuItem>Delete</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	)
-}
